9daa10a6bb16cc051ac2cae89e73f9a9890a0bda
s1monj <simon@whitebrick.com>
2024-12-02 12:53:58 -0500

    feat: add host to grant code

--- jhe/core/views/patient.py
+++ jhe/core/views/patient.py
@@ -56,7 +56,7 @@ class PatientViewSet(ModelViewSet):
     def invitation_link(self, request, pk):
         patient = self.get_object()
         grant = patient.jhe_user.create_authorization_code(1,settings.OIDC_CLIENT_REDIRECT_URI)
-        return Response({"invitation_link": settings.CH_INVITATION_LINK_PREFIX+grant.code})
+        return Response({"invitation_link": settings.CH_INVITATION_LINK_PREFIX+settings.SITE_URL.split('/')[2]+'|'+grant.code})
 
     @action(detail=True, methods=['GET','POST','PATCH','DELETE'])
     def consents(self, request, pk):
