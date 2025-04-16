5dc4176d881bbb231f659e72d22020941f9888a9
travis-sauer-oltech <travis.sauer@orbitlinktech.com>
2025-04-15 20:32:46 -0500

    chore(workflow): ✨ update PostgreSQL service configuration in GitHub Actions
    * Added PostgreSQL service with health checks for the test suite.
    * Updated database settings to use environment variables for better configuration management.
    * Changed database engine from SQLite to PostgreSQL for consistency with production.

--- jhe/jhe/test_settings.py
+++ jhe/jhe/test_settings.py
@@ -1,8 +1,21 @@
-from jhe.settings import *
+import os
+from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',  # In-memory database
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", "test_jhe_dev"),
+        'USER': os.environ.get("DB_USER", "postgres"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'PORT': os.environ.get("DB_PORT", "5432"),
     }
 }
+# Disable migrations for the oauth2_provider app to speed up tests
+# This is a common practice in testing to avoid running migrations on the test database.
+# It assumes that the app is already migrated in the main database.
+MIGRATION_MODULES = {
+    'oauth2_provider': None,
+}
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
