346737bf18bbacded7ec1c14551a27bac363bcd1
travis-sauer-oltech <travis.sauer@orbitlinktech.com>
2025-04-19 14:26:28 -0500

    chore(test_settings): ✨ remove migration modules for oauth2_provider
    * Disabled migrations for the `oauth2_provider` app to speed up tests.
    * This change assumes that the app is already migrated in the main database.

--- jhe/jhe/test_settings.py
+++ jhe/jhe/test_settings.py
@@ -11,11 +11,5 @@ DATABASES = {
         'PORT': os.environ.get("DB_PORT", "5432"),
     }
 }
-# Disable migrations for the oauth2_provider app to speed up tests
-# This is a common practice in testing to avoid running migrations on the test database.
-# It assumes that the app is already migrated in the main database.
-MIGRATION_MODULES = {
-    'oauth2_provider': None,
-}
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
