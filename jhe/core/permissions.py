40b9258e507e8b7b674bcca21cffb718afa274db
s1monj <simon@whitebrick.com>
2025-08-13 00:10:32 -0400

    chore: IsOrganizationManager no longer used

--- jhe/core/permissions.py
+++ jhe/core/permissions.py
@@ -13,27 +13,6 @@ class IsSelfUrlPath(permissions.BasePermission):
         return int(request.parser_context["kwargs"]["pk"]) == request.user.id
 
 
-class IsOrganizationManager(permissions.IsAuthenticated):
-    """
-    Only allows organization managers to add and delete the practitioners.
-    """
-
-    def has_permission(self, request, view):
-        """
-        Return True if the user is an organization manager or False if not.
-        Also checks the authenticated user.
-        """
-
-        if super().has_permission(request, view):
-            if org_id := view.kwargs.get("pk"):
-                return PractitionerOrganization.objects.filter(
-                    practitioner__jhe_user=request.user,
-                    organization_id=org_id,
-                    role=PractitionerOrganization.ROLE_MANAGER,
-                ).exists()
-        return False
-
-
 # RBAC
 ROLE_PERMISSIONS = {
     "super_user": [
